477ca2db8fb5cc540366186ca8aa12a981d49a16
Max Prosper <maxim.yaaman@gmail.com>
2017-10-23 00:00:41 +0300

    Server side data loading implemented

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/Routes';
 import createStore from './helpers/createStore';
 
 const app = express();
@@ -10,10 +12,14 @@ app.use(express.static('public'));
 app.get('*', (req, res) => {
   const store = createStore();
 
-  //Some logic to initialize and load data into the store
+  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
+    return route.loadData ? route.loadData(store) : null;
+  });
 
-  res.send(renderer(req, store));
-})
+  Promise.all(promises).then(() => {
+    res.send(renderer(req, store));
+  });
+});
 
 app.listen(3010, () => {
   console.log('Listening on port 3010')
